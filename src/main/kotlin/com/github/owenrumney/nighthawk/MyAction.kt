40c7494b2d1118907a373088602647e47bc1c8b5
GitHub Action <action@github.com>
2023-08-03 13:21:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/owenrumney/nighthawk/MyAction.kt
+++ src/main/kotlin/com/github/owenrumney/nighthawk/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.owenrumney.nighthawk
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
